7b9a459d814fbf41d688c99088291b866a867c97
TieMuo <596139547@qq.com>
2023-03-26 19:10:25 +0800

    增删改查

--- springboot_vue_wms/src/main/java/com/mba/controller/UserController.java
+++ springboot_vue_wms/src/main/java/com/mba/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.mba.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.mba.entity.User;
+import com.mba.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +20,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private IUserService userService;
+    @GetMapping("/list")
+    public List<User> List(){
+        return userService.list();
+    }
 
+    //新增
+    @PostMapping("/save")
+    public boolean save(@RequestBody User user){
+        return userService.save(user);
+    }
+    //修改
+    @PostMapping("/mod")
+    public boolean mod(@RequestBody User user){
+        return userService.updateById(user);
+    }
+    //新增或修改
+    @PostMapping("/saveOrMod")
+    public boolean saveOrMod(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+    //删除
+    @GetMapping("/delete")
+    public boolean delete(Integer id){
+        return userService.removeById(id);
+    }
+    //查询
+    @PostMapping("/listP")
+    public List<User> listP(@RequestBody User user){
+        LambdaQueryWrapper<User> lambdaQueryWrapper =new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.like(User::getName,user.getName());
+        return userService.list(lambdaQueryWrapper);
+    }
 }
